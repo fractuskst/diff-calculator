d217aa7b4a2f0fe4fb65377c48bc35c882092d3c
Konstantin Kovalyov <fractuskst@yandex.ru>
2024-04-23 22:14:44 +0300

    add diff func

---
+++ src/diff.js
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import parseFile from './parse.js';
+import _ from 'lodash';
+
+const genDiff = (filepath1, filepath2) => {
+  let result = '';
+
+  const file1 = fs.readFileSync(filepath1, 'utf-8');
+  const file2 = fs.readFileSync(filepath2, 'utf-8');
+
+  const json1 = parseFile(file1);
+  const json2 = parseFile(file2);
+
+  const keys = _.union(Object.keys(json1), Object.keys(json2));
+  const sortedKeys = _.sortBy(keys);
+
+  sortedKeys.forEach((key) => {
+    if (_.has(json1, key) && _.has(json2, key)) {
+      if (json1[key] === json2[key]) {
+        result += `${key}: ${json1[key]}\n`;
+      } else {
+        result += `- ${key}: ${json1[key]}\n`;
+        result += `+ ${key}: ${json2[key]}\n`;
+      }
+    } else if (_.has(json2, key)) {
+      result += `+ ${key}: ${json2[key]}\n`;
+    } else {
+      result += `- ${key}: ${json1[key]}\n`;
+    }
+  });
+  return result;
+};
+export default genDiff;
