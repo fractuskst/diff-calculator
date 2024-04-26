29d18dc91d7a384faa3e08efb95aecd86099b68a
Konstantin Kovalyov <fractuskst@yandex.ru>
2024-04-26 20:52:11 +0300

    lint refactoring

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,14 +5,14 @@ import genDiff from '../src/diff.js';
 import convertPath from '../src/path.js';
 
 program
-  .description("Compares two configuration files and shows a difference.")
+  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .option('-f, --format [type], output format')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
     const absPath1 = convertPath(filepath1);
-    const absPath2 = convertPath(filepath2); 
+    const absPath2 = convertPath(filepath2);
     const difference = genDiff(absPath1, absPath2);
     console.log(difference);
   });
--- src/diff.js
+++ src/diff.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
-import parseFile from './parse.js';
 import _ from 'lodash';
+import parseFile from './parse.js';
 
 const genDiff = (filepath1, filepath2) => {
   let result = '';
