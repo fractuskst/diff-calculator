ff55cd2eae8c525cb68672bfdf0a75807d1bf49d
Konstantin Kovalyov <fractuskst@yandex.ru>
2024-04-27 11:59:49 +0300

    refactor tests

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,13 +1,13 @@
-import genDiff from '../src/diff.js';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import fs from 'fs';
+// import fs from 'fs';
+import genDiff from '../src/diff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+// const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gendiff', () => {
   const file1 = getFixturePath('file1.json');
@@ -19,7 +19,5 @@ test('gendiff', () => {
   - timeout: 50
   + timeout: 20
   + verbose: true
-}`)
+}`);
 });
-
-
