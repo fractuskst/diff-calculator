ae1e3a5fcde195cc82e5de969dbfc1db0e9f85e4
Konstantin Kovalyov <fractuskst@yandex.ru>
2024-04-28 16:50:07 +0300

    add yaml files and update tests

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,23 +1,24 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
-// import fs from 'fs';
+import fs from 'fs';
 import genDiff from '../src/diff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-// const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-  expect(genDiff(file1, file2)).toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
+const expected = readFile('expectedResult.txt');
+const json1 = getFixturePath('file1.json');
+const json2 = getFixturePath('file2.json');
+const yaml1 = getFixturePath('file1.yaml');
+const yaml2 = getFixturePath('file2.yaml');
+
+test('gendiff JSON', () => {
+  expect(genDiff(json1, json2)).toEqual(expected);
+});
+
+test('gendiff YAML', () => {
+  expect(genDiff(yaml1, yaml2)).toEqual(expected);
 });
