a1ed94fa9cf3c02e12d53fec32b09158e806b2fa
Konstantin Kovalyov <fractuskst@yandex.ru>
2024-05-02 14:01:51 +0300

    change data at __fixtures__

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -10,15 +10,15 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const expected = readFile('expectedResult.txt');
-const json1 = getFixturePath('file1.json');
-const json2 = getFixturePath('file2.json');
-const yaml1 = getFixturePath('file1.yaml');
-const yaml2 = getFixturePath('file2.yaml');
 
 test('gendiff JSON', () => {
+  const json1 = getFixturePath('file1.json');
+  const json2 = getFixturePath('file2.json');
   expect(genDiff(json1, json2)).toEqual(expected);
 });
 
 test('gendiff YAML', () => {
+  const yaml1 = getFixturePath('file1.yaml');
+  const yaml2 = getFixturePath('file2.yaml');
   expect(genDiff(yaml1, yaml2)).toEqual(expected);
 });
