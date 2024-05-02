01ae3933aeda42e162ecddb0479a133f24a31eb8
Konstantin Kovalyov <fractuskst@yandex.ru>
2024-05-02 14:01:58 +0300

    refactoring

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,10 +6,10 @@ import genDiff from '../src/diff.js';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format [type], output format')
+  .option('-f, --format <type>, output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 program.parse(process.argv);
--- src/diff.js
+++ src/diff.js
@@ -1,38 +1,36 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
 import path from 'path';
+import { cwd } from 'process';
 import _ from 'lodash';
 import parse from './parsers.js';
 
 const getData = (filepath) => {
-  const absolutePath = path.resolve(process.cwd(), filepath);
-  return fs.readFileSync(absolutePath, 'utf-8');
+  const absolutePath = path.resolve(cwd(), filepath);
+  return readFileSync(absolutePath, 'utf-8');
 };
 
 const getFormat = (filepath) => path.extname(filepath);
 
 const genDiff = (filepath1, filepath2) => {
-  let result = '';
-
   const file1 = parse(getData(filepath1), getFormat(filepath1));
   const file2 = parse(getData(filepath2), getFormat(filepath2));
 
   const keys = _.union(Object.keys(file1), Object.keys(file2));
   const sortedKeys = _.sortBy(keys);
 
-  sortedKeys.forEach((key) => {
-    if (_.has(file1, key) && _.has(file2, key)) {
-      if (file1[key] === file2[key]) {
-        result += `    ${key}: ${file1[key]}\n`;
-      } else {
-        result += `  - ${key}: ${file1[key]}\n`;
-        result += `  + ${key}: ${file2[key]}\n`;
-      }
-    } else if (_.has(file2, key)) {
-      result += `  + ${key}: ${file2[key]}\n`;
-    } else {
-      result += `  - ${key}: ${file1[key]}\n`;
+  const diff = sortedKeys.map((key) => {
+    if (_.has(file1, key) && !_.has(file2, key)) {
+      return `  - ${key}: ${file1[key]}`;
     }
+    if (!_.has(file1, key) && _.has(file2, key)) {
+      return `  + ${key}: ${file2[key]}`;
+    }
+    if (_.has(file1, key) && _.has(file2, key) && file1[key] === file2[key]) {
+      return `    ${key}: ${file1[key]}`;
+    }
+    return `  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
   });
-  return `{\n${result}}`;
+  return ['{', ...diff, '}'].join('\n');
 };
+
 export default genDiff;
