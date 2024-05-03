592c8a7a379d09e04644877ab7fbdff8ba295515
Konstantin Kovalyov <fractuskst@yandex.ru>
2024-05-03 15:07:30 +0300

    update gendiff and add new asciinema

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ import genDiff from '../src/diff.js';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>, output format', 'stylish')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
